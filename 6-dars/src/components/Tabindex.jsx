90fbb9020ccc7613005a8c9f97c6bf94c7971565
anvarbeknorimov <anvarbeknorimov9846@gmail.com>
2023-07-24 10:52:18 +0500

    title

--- 6-dars/src/components/Tabindex.jsx
+++ 6-dars/src/components/Tabindex.jsx
@@ -1,59 +1,6 @@
 import { useState } from "react";
-import Main from "./Main"
+import Main from "./Main";
 import "../css/Tabindex.css";
-
-// const Tabindex = () => {
-//   const [contName, setContName] = useState("positive");
-//   const handleChange = (e) => {
-//     let count =
-//       e.target.innerText.toLowerCase().split("")[0].toUpperCase() +
-//       e.target.innerText.toLowerCase().split("").splice(1).join("");
-//     setContName(count);
-//   };
-//   const handleInput = (e) => {
-//     e.preventDefault();
-//     console.log(e.target.children[0].value);
-//     if (e.target.children[0].value == 1) {
-//       setContName("positive");
-//     } else if (e.target.children[0].value == 2) {
-//       setContName("Negative");
-//     } else if (e.target.children[0].value == 3) {
-//       setContName("Neutral");
-//     } else {
-//       alert("Not Found");
-//     }
-//   };
-//   return (
-//     <div className="container">
-//       <div className="section">
-//         <form onSubmit={handleInput} className="section__top">
-//           <input
-//             type="text"
-//             placeholder="Enter tab index"
-//             className="section__top-input"
-//           />
-//           <button className="section__top-button">Change tab</button>
-//         </form>
-//         <div className="section__bottom">
-//           <div className="section__bottom-title">
-//             <span onClick={handleChange} className="title ">
-//               POSITIVE
-//             </span>
-//             <span onClick={handleChange} className="title ">
-//               NEGATIVE
-//             </span>
-//             <span onClick={handleChange} className="title">
-//               NEUTRAL
-//             </span>
-//           </div>
-//           <span className="section__bottom-subtitle">
-//             {<p>{contName} content</p>}
-//           </span>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 const TabIndex = () => {
   const [contName, setContName] = useState("Positive");
   const [showModal, setShowModal] = useState(true);
@@ -95,7 +42,12 @@ const TabIndex = () => {
           className="section__top "
           placeholder="Enter tab index"
         >
-          <input name="tabIndex" type="text" placeholder="Enter tab index" className="section__top-input"/>
+          <input
+            name="tabIndex"
+            type="text"
+            placeholder="Enter tab index"
+            className="section__top-input"
+          />
           <button type="submit" className=" section__top-button">
             Change tab
           </button>
